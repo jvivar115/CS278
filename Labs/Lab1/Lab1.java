94fc70e75dae18bc4ce852cc9212e5185cc65042
jvivar <jvivar98@nmsu.edu>
2020-09-15 10:44:53 -0600

    Whoa

--- Labs/Lab1/Lab1.java
+++ Labs/Lab1/Lab1.java
@@ -12,77 +12,77 @@ public class Lab1 {
 		int count = 1;
 		
 		// 8 for loops create truth tables, eliminating one possibility at a time. 
-		for ( int prop1 = 0; prop1 <= 1; prop1++ ) {
-			for ( int prop2 = 0; prop2 <= 1; prop2++ ) {
-				for ( int prop3 = 0; prop3 <= 1; prop3++) {
-					for ( int prop4 = 0; prop4 <= 1; prop4++ ) {
-						for ( int prop5 = 0; prop5 <= 1; prop5++ ) {
-							for ( int prop6 = 0; prop6 <= 1; prop6++ ) {
-								for ( int prop7 = 0; prop7 <= 1; prop7++ ) {
-									for ( int prop8 = 0; prop8 <= 1; prop8++ ) {
+		for ( int proposition_one = 0; proposition_one <= 1; proposition_one++ ) {
+			for ( int proposition_two = 0; proposition_two <= 1; proposition_two++ ) {
+				for ( int proposition_three = 0; proposition_three <= 1; proposition_three++) {
+					for ( int proposition_four = 0; proposition_four <= 1; proposition_four++ ) {
+						for ( int proposition_five = 0; proposition_five <= 1; proposition_five++ ) {
+							for ( int proposition_six = 0; proposition_six <= 1; proposition_six++ ) {
+								for ( int proposition_seven = 0; proposition_seven <= 1; proposition_seven++ ) {
+									for ( int proposition_eight = 0; proposition_eight <= 1; proposition_eight++ ) {
 										
-										System.out.printf("Truth Table %d:\np   q   r   proposition\n", count++);
+										System.out.printf("Truth Table %d:\np   q   r   Proposition\n", count++);
 										System.out.printf("-----------------\n");
 										
-										// Converts of prop variables into 'T' or 'F' format.
-										if ( prop1 == 1 ) {
+										// Prints out the true or false statements and proposition results
+										if ( proposition_one == 1 ) {
 											System.out.print( "F   F   F   T\n" );
 										} else {
 											System.out.print( "F   F   F   F\n" );
-										} // end prop1 if
+										} // end proposition_one if
 										
-										if ( prop2 == 1 ) {
+										if ( proposition_two == 1 ) {
 											System.out.print( "F   F   T   T\n");
 										} else {
 											System.out.print( "F   F   T   F\n");
-										} // end prop2 if
+										} // end proposition_two if
 										
-										if ( prop3 == 1 ) {
+										if ( proposition_three == 1 ) {
 											System.out.print( "F   T   F   T\n");
 										} else {
 											System.out.print( "F   T   F   F\n");
-										} // end prop3 if
+										} // end proposition_three if
 										
-										if ( prop4 == 1 ) {
+										if ( proposition_four == 1 ) {
 											System.out.print( "F   T   T   T\n");
 										} else {
 											System.out.print( "F   T   T   F\n");
-										} // end prop4 if
+										} // end proposition_four if
 										
-										if ( prop5 == 1 ) {
+										if ( proposition_five == 1 ) {
 											System.out.print( "T   F   F   T\n");
 										} else {
 											System.out.print( "T   F   F   F\n");
-										} // end prop5 if
+										} // end proposition_five if
 										
-										if ( prop6 == 1 ) {
+										if ( proposition_six == 1 ) {
 											System.out.print( "T   F   T   T\n");
 										} else {
 											System.out.print( "T   F   T   F\n");
-										} // end prop6 if
+										} // end proposition_six if
 										
-										if ( prop7 == 1 ) {
+										if ( proposition_seven == 1 ) {
 											System.out.print( "T   T   F   T\n");
 										} else {
 											System.out.print( "T   T   F   F\n");
-										} // end prop7 if
+										} // end proposition_seven if
 										
-										if ( prop8 == 1 ) {
+										if ( proposition_eight == 1 ) {
 											System.out.print( "T   T   T   T\n");
 										} else {
 											System.out.print( "T   T   T   F\n");
-										} // end prop 8 if
+										} // end proposition_eight if
 										
 										System.out.println("");
 										
-									} // end prop8 for
-								} // end prop7 for
-							} // end prop6 for
-						} // end prop5 for
-					} // end prop4 for
-				} // end prop3 for
-			} // end prop2 for
-		} // end prop1 for
+									} // end proposition_eight for
+								} // end proposition_seven for
+							} // end proposition_six for
+						} // end proposition_five for
+					} // end proposition_four for
+				} // end proposition_three for
+			} // end proposition_two for
+		} // end proposition_one for
 
 	} // end main
 } // end class
